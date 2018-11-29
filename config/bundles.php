d6dbbb68d07bd68d2a067dde824b071dbf41e24c
Kévin Dunglas <dunglas@gmail.com>
2018-11-29 18:23:01 +0100

    Basic website

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SessionController extends AbstractController
 {
     /**
-     * @Route("/session", name="session")
+     * @Route("/", name="session_index")
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(Session::class);
+
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $repository->findBy([], ['datetime' => 'ASC'])
         ]);
+    }
+
+    /**
+     * @Route("/session/{id}", name="session_detail")
+     */
+    public function detail(Session $session)
+    {
+        return $this->render('session/detail.html.twig', ['session' => $session]);
     }
 }
