51e0a689fc20ec2073d9f90b216e5e2d3077e622
Kévin Dunglas <dunglas@gmail.com>
2018-12-07 10:31:46 +0000

    Fix tests

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -15,13 +15,13 @@ class SessionTest extends PantherTestCase
     {
         $client = static::createPantherClient();
         $crawler = $client->request('GET', '/');
-        $this->assertSame('SymfonyCon Schedule', $crawler->filter('h1:first-of-type')->text());
+        $this->assertSame('Schedule', $crawler->filter('h1:first-of-type')->text());
 
         $crawler = $client->clickLink(self::SESSION_TITLE);
-        $this->assertSame(self::SESSION_TITLE, $crawler->filter('h1:first-of-type')->text());
+        $this->assertSame(self::SESSION_TITLE, $crawler->filter('article h1:first-of-type')->text());
 
         $client->waitFor('#no-comments');
-        $this->assertSame('No feedback yet 🙁!', $crawler->filter('#no-comments')->text());
+        $this->assertSame('No feedback yet !', $crawler->filter('#no-comments')->text());
 
         $client->getMouse()->clickTo('.vue-star-rating-pointer:nth-of-type(5)');
         $crawler = $client->submitForm('Post', [
@@ -29,8 +29,8 @@ class SessionTest extends PantherTestCase
             'comment' => 'I hope you\'re enjoying the talk!',
         ]);
 
-        $client->waitFor('#comments li');
-        $this->assertContains('I hope you\'re enjoying the talk!', $crawler->filter('#comments li')->text());
+        $client->waitFor('#comments article p');
+        $this->assertContains('I hope you\'re enjoying the talk!', $crawler->filter('#comments article p')->text());
         $this->assertEmpty($crawler->filter('#no-comments'));
 
         $client->takeScreenshot('/tmp/panther.png');
